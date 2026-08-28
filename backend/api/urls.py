aefc160690335cf8cb69303d49d6c1383dff28c2
Rani Kellock <ranikellock@Ranis-MacBook-Air.local>
2026-08-28 11:51:53 +1000

    deleted unnecesary 'hello world' path :)'

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello_world, name="hello_world"),
     path('create_random_seed/', views.create_random_seed, name="create_random_seed"),
     path('randomise_arcana/', views.randomise_arcana, name="randomise_arcana"),
     path('randomise_arcana/<str:seed>/', views.randomise_arcana, name="randomise_arcana"),
